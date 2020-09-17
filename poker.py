0f3a7c342e29e40d62fb69a675cdb56cd35c65ed
messyv <71394934+messyv@users.noreply.github.com>
2020-09-17 19:03:56 +0200

    Add files via upload

--- poker.py
+++ poker.py
@@ -14,3 +14,12 @@ print (szam4)
 
 szam5 = random.randrange( 6) + 1
 print (szam5)
+
+print (
+	"| %6s" % "Gép:",
+	"%d" % szam1,
+	"%d" % szam2,
+	"%d" % szam3,
+	"%d" % szam4,
+	"%d |" % szam5,
+	)
