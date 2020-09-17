4ee610e8ca0077a10674d4856eafe98555da695a
messyv <71394934+messyv@users.noreply.github.com>
2020-09-17 19:47:56 +0200

    Add files via upload

---
+++ konvert.py
@@ -0,0 +1,17 @@
+szam = 35.5421
+print (type(szam))
+
+szam2 = "35.5421"
+print (type(szam2))
+
+szam3 = 35
+print (type(szam3))
+
+szam4 = 35.
+print (type(szam4))
+
+szam5 = int(35.0)
+print (type(szam5))
+
+szam6 = str(35.0)
+print (type(szam6))
