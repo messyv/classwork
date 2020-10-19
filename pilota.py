5dae00cdf38d5cd688808044964ec5a3aaeef52f
messyv <71394934+messyv@users.noreply.github.com>
2020-10-19 17:07:37 +0200

    Add files via upload

---
+++ pilota.py
@@ -0,0 +1,16 @@
+rovid = input("Engedély rövidítés:")
+
+
+LAPL = "Angolul: Light Aircraft Pilot Licence\nMagyarul: Könnyű Repülőgép Pilóta Szakszolgálati Engedély"
+ATPL = "Angolul: Airline Transport Pilot Licence\nMagyarul: Légitársasági Pilóta Szakszolgálati Engedély"
+PPL = "Angolul: Private Pilot Licence\nMagyarul: Magánpilóta Szakszolgálati Engedély"
+CPL = "Angolul: Commercial Pilot Licence\nMagyarul: Kereskedelmi Pilóta Szakszolgálati Engedély"
+
+if rovid == "LAPL":
+	print("Angolul: Light Aircraft Pilot Licence\n Magyarul: Könnyű Repülőgép Pilóta Szakszolgálati Engedély")
+if rovid == "ATPL":
+	print("Angolul: Airline Transport Pilot Licence\n Magyarul: Légitársasági Pilóta Szakszolgálati Engedély")
+if rovid == "PPL":
+	print("Angolul: Private Pilot Licence\n Magyarul: Magánpilóta Szakszolgálati Engedély")
+if rovid == "CPL":
+	print("Angolul: Commercial Pilot Licence\n Magyarul: Kereskedelmi Pilóta Szakszolgálati Engedély")
